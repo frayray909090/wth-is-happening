a50ff22762b96ebb805073d85719b6fffa006327
Make hacks for iOS <67075721+frayray909090@users.noreply.github.com>
2023-02-23 10:58:17 -0800

    Add files via upload

---
+++ unfinished-Windows-app-store-WPF-main/unfinished-Windows-app-store-WPF-main/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Store_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        void UIElement_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+        void Close_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+        void Home(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+        void Featured(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+        void Mine(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+        void Malware(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+        void Other(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ unfinished-Windows-app-store-WPF-main/unfinished-Windows-app-store-WPF-main/README.md
@@ -0,0 +1,7 @@
+i give permission to people to edit it finish it and shit
+
+just remember im 14 and no i did not put the app downloding code in it i wont people can try and figure it ot
+
+merry fucking chrismas 
+
+i will not answer to bugs or reports im not doing anything with this
