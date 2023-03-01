4919dba7eb472c9b00ffdde728a38a88b49a3fba
Make hacks for iOS <67075721+frayray909090@users.noreply.github.com>
2023-03-01 11:25:32 -0800

    Add files via upload

---
+++ New folder/yea.ino
@@ -0,0 +1,70 @@
+// define the LED digit patterns, from 0 - 9
+// 1 = LED on, 0 = LED off, in this order:
+//                74HC595 pin     Q0,Q1,Q2,Q3,Q4,Q5,Q6,Q7 
+//                Mapping to      a,b,c,d,e,f,g of Seven-Segment LED
+byte seven_seg_digits[10] = { B11111100,  // = 0
+                              B01100000,  // = 1
+                              B11011010,  // = 2
+                              B11110010,  // = 3
+                              B01100110,  // = 4
+                              B10110110,  // = 5
+                              B10111110,  // = 6
+                              B11100000,  // = 7
+                              B11111110,  // = 8
+                              B11100110   // = 9
+                             };
+ 
+// connect to the ST_CP of 74HC595 (pin 3,latch pin)
+int latchPin = 3;
+// connect to the SH_CP of 74HC595 (pin 4, clock pin)
+int clockPin = 4;
+// connect to the DS of 74HC595 (pin 2)
+int dataPin = 2;
+
+// Arduino pin numbers
+const int SW_pin = 2; // digital pin connected to switch output
+const int X_pin = 0; // analog pin connected to X output
+const int Y_pin = 1; // analog pin connected to Y output
+
+void setup() {
+  pinMode(SW_pin, INPUT);
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  digitalWrite(SW_pin, HIGH);
+  Serial.begin(9600);
+}
+void sevenSegWrite(byte digit) {
+  // set the latchPin to low potential, before sending data
+  digitalWrite(latchPin, LOW);
+     
+  // the original data (bit pattern)
+  shiftOut(dataPin, clockPin, LSBFIRST, seven_seg_digits[digit]);  
+ 
+  // set the latchPin to high potential, after sending data
+  digitalWrite(latchPin, HIGH);
+}
+void loop() {
+  Serial.print("Switch:  ");
+  Serial.print(digitalRead(SW_pin));
+  Serial.print("\n");
+  Serial.print("X-axis: ");
+  Serial.print(analogRead(X_pin));
+  Serial.print("\n");
+  Serial.print("Y-axis: ");
+  Serial.println(analogRead(Y_pin));
+  Serial.print("\n\n");
+  delay(500);
+  if (Serial.print("Switch: 0")) {
+    sevenSegWrite(B11111100); 
+    Serial.print("Switch Down");
+  }
+  if (Serial.print("Y-axis: 0")) {
+    sevenSegWrite(B11111100); 
+    Serial.print("Y 1 Active");
+  }
+  if (Serial.print("X-axis: 0")) {
+    sevenSegWrite(B11111100);
+    Serial.print("X 1 Active");
+  }
+}
